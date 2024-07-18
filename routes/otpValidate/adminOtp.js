4a39a351015a4ae9cd9ce8f5a042ce676727f92d
JeevaNandham9843 <jeevanandham9843@gmail.com>
2024-07-18 18:33:25 +0530

    add folders

--- routes/otpValidate/adminOtp.js
+++ routes/otpValidate/adminOtp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const Admin = require('../models/adminRegistration');
+const Admin = require('../../models/adminRegistration');
 
 router.post('/login', async (req, res, next) => {
     const { username, password } = req.body;
